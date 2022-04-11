f3a5fda3ad211654a8c0bfca20fe339fea1d2570
Nick Sadler <nicksadler14@gmail.com>
2022-04-10 20:03:30 -0400

    force build

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const appOnlyClient = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
 app.get("/api", async (req, res) => {
   try {
     const response = await appOnlyClient.v2.tweetCountRecent("yankees")
-    console.log(response.meta.total_tweet_count)
+    
     res.json({ message: response.meta.total_tweet_count});
   } catch (e) {
     console.log(e)
